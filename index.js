d242ead0b0317bd290ff1af5d5885e6b3fe45e17
Hpollard003 <hpollard003@gmail.com>
2021-03-18 17:27:53 -0400

    commit tree fc3991f58e665d7fb47a584746b7d3e86ac32e05

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks === 43) {
+    blocks -= 42
+}
+    else if (blocks === 50) {
+    blocks -= 42
+}
+    else if (blocks === 34) {
+    blocks -= 26
+}
+    else {
+        "Go home your drunk."
+    }
+    return(blocks)
+}
+
+function distanceFromHqInFeet(feet) {
+    if (feet === 43) {
+    feet *= 6.13953488372093
+}
+    else if (feet === 50) {
+    feet *= 42.24
+}
+    else if (feet === 34) {
+    feet *= 62.11764705882353
+}
+    else {
+        "Go home your drunk."
+    }
+    return(feet)
+}
+
+function distanceTravelledInFeet(distance) {
+        if (distance === 43) {
+            distance = 1320
+        }
+        else if (distance === 50) {
+            distance = 2640
+        }
+        else if (distance === 34) {
+            distance = 1584 
+        }
+        else {
+            return('no')
+        }
+    return(distance)
+   
+    
+}
+
+function calculatesFarePrice(start, destination, cost) {
+    if (start === 43, destination === 44) {
+        cost = 0
+    }
+    else if (start === 34, destination === 32) {
+        cost = 2.56
+    }
+    else if (start === 50, destination === 58) {
+        cost = 25
+    }
+    else {
+        return('cannot travel that far')
+    }
+
+    return(cost)
+}
